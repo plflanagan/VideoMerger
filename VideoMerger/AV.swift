ee1519f0784fc46ba143dc8f13c07fca91a0e795
Patrick Flanagan <patrick@unum.la>
2019-04-23 12:36:32 -0700

    Make video

---
+++ VideoMerger/AV.swift
@@ -0,0 +1,108 @@
+////
+////  AVPlayerLayer.swift
+////  UNUMCanvas
+////
+////  Created by Patrick Flanagan on 4/2/19.
+////
+//
+//import Foundation
+//import AVKit
+//import Anchorage
+//
+//public enum PlayerViewFactory {
+//    public static func makePlayerView(with avAsset: AVAsset) -> AVPlayerView {
+//        let avPlayerItem = AVPlayerItem(asset: avAsset)
+//        let avPlayer = AVPlayer(playerItem: avPlayerItem)
+//
+//        let mediaView = AVPlayerView(player: avPlayer)
+//
+//        if let videoLayer = mediaView.layer as? AVPlayerLayer {
+//            videoLayer.backgroundColor = UIColor.clear.cgColor
+//            videoLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+//        }
+//
+//        return mediaView
+//    }
+//}
+//
+//public class AVPlayerView: UIView {
+//
+//    private var videoView: VideoView?
+//
+//    public var videoPlayer: AVPlayer {
+//        guard let videoView = videoView else {
+//            assertionFailure("Player Layer should be set at init.")
+//            return AVPlayer(playerItem: nil)
+//        }
+//        return videoView.videoPlayer
+//    }
+//
+//    public init(player: AVPlayer) {
+//        super.init(frame: .zero)
+//
+//        let image = getImage(from: player)
+//        let imageView = UIImageView(image: image)
+//        addSubview(imageView)
+//        imageView.edgeAnchors == edgeAnchors
+//
+//        let videoView = VideoView(player: player)
+//        addSubview(videoView)
+//        videoView.edgeAnchors == edgeAnchors
+//        self.videoView = videoView
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    private func getImage(from player: AVPlayer) -> UIImage? {
+//        guard let asset = player.currentItem?.asset else {
+//            return nil
+//        }
+//
+//        let assetIG = AVAssetImageGenerator(asset: asset)
+//        assetIG.appliesPreferredTrackTransform = true
+//        assetIG.apertureMode = AVAssetImageGenerator.ApertureMode.encodedPixels
+//
+//        let cmTime = CMTime(seconds: 0, preferredTimescale: 60)
+//        let thumbnailImageRef: CGImage
+//        do {
+//            thumbnailImageRef = try assetIG.copyCGImage(at: cmTime, actualTime: nil)
+//        } catch let error {
+//            print("Error: \(error)")
+//            return nil
+//        }
+//
+//        return UIImage(cgImage: thumbnailImageRef)
+//    }
+//}
+//
+//private class VideoView: UIView {
+//    public var videoPlayer: AVPlayer {
+//        guard
+//            let playerLayer = layer as? AVPlayerLayer,
+//            let player = playerLayer.player
+//            else {
+//                assertionFailure("Player Layer should be set at init.")
+//                return AVPlayer(playerItem: nil)
+//        }
+//        return player
+//    }
+//
+//    override public class var layerClass: AnyClass {
+//        return AVPlayerLayer.self
+//    }
+//
+//    public init(player: AVPlayer) {
+//        super.init(frame: .zero)
+//        guard let castedLayer = layer as? AVPlayerLayer else {
+//            assertionFailure("Layer is not able to be cast properly")
+//            return
+//        }
+//        castedLayer.player = player
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}
--- VideoMerger/ViewController.swift
+++ VideoMerger/ViewController.swift
@@ -10,86 +10,93 @@ import UIKit
 import AVFoundation
 import AVKit
 
-class ViewController: UIViewController {
+class VideoComposer {
+    let composition = AVMutableComposition()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        overlapVideos()
-    }
+    // make main video instruction
+    let mainInstruction = AVMutableVideoCompositionInstruction()
 
-    func overlapVideos() {
-        let startTime = DispatchTime.now()
-
-        let composition = AVMutableComposition()
-
-        // make main video instruction
-        let mainInstruction = AVMutableVideoCompositionInstruction()
-
-        guard let pathUrl = Bundle.main.url(forResource: "SampleVideo_1280x720_1mb", withExtension: "mp4") else {
-            assertionFailure()
-            return
-        }
-
-
-        // make first video track and add to composition
-        let firstAsset = AVAsset(url: pathUrl)
+    func overlapVideos(avAsset: AVAsset, completion: @escaping (AVAssetExportSession) -> Void) {
 
         // timeframe will match first video for this example
-        mainInstruction.timeRange = CMTimeRangeMake(start: .zero, duration: firstAsset.duration)
+        mainInstruction.timeRange = CMTimeRangeMake(start: .zero, duration: avAsset.duration)
+
+        add(avAsset: avAsset)
 
 
-        guard let firstTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else {
-            assertionFailure()
-            return
-        }
-        try! firstTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: firstAsset.duration), of: firstAsset.tracks(withMediaType: .video)[0], at: .zero)
+//        guard let secondTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else {
+//            assertionFailure()
+//            return
+//        }
+//        try! secondTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: secondAsset.duration), of: secondAsset.tracks(withMediaType: .video)[0], at: .zero)
+//
+//        // add layer instruction for second video
+//        let secondVideoLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: secondTrack)
+//        let secondMove = CGAffineTransform(translationX: -500, y: -500)
+//        let secondScale = CGAffineTransform(scaleX: 0.2, y: 0.2)
+//
+//        secondVideoLayerInstruction.setTransform(secondMove.concatenating(secondScale), at: .zero)
+//
+//        mainInstruction.layerInstructions.append(secondVideoLayerInstruction)
+//
+//
+//        // make third video track and add to composition
+//        let thirdAsset = AVAsset(url: pathUrl)
+//
+//        guard let thirdTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else {
+//            assertionFailure()
+//            return
+//        }
+//        try! thirdTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: thirdAsset.duration), of: thirdAsset.tracks(withMediaType: .video)[0], at: .zero)
+//
+//        // add layer instruction for third video
+//        let thirdVideoLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: thirdTrack)
+//        let thirdMove = CGAffineTransform(translationX: 1000, y: 0)
+//        let thirdScale = CGAffineTransform(scaleX: 0.2, y: 0.2)
+//        let thirdRotate = CGAffineTransform(rotationAngle: 20)
+//
+//        thirdVideoLayerInstruction.setTransform(thirdMove.concatenating(thirdScale).concatenating(thirdRotate), at: .zero)
+//
+//        mainInstruction.layerInstructions.append(thirdVideoLayerInstruction)
+//
+//
+//        // add image layer
+//        let image = UIImage(named: "image")!
+//
+//        let movieLength = TimeInterval(firstAsset.duration.seconds)
+//        let searchPaths1 = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+//        let documentDirectory1 = searchPaths1[0]
+//        let filePath1 = documentDirectory1.appending("output1.mov")
+//        let outputUrl1 = URL(fileURLWithPath: filePath1)
+//
+//
+//
+//        ImageVideoCreator.writeSingleImageToMovie(image: image, movieLength: movieLength, outputFileURL: outputUrl1) { success in
+//            print("success: \(success)")
+//
+//            // make first video track and add to composition
+//            let imageAsset = AVAsset(url: outputUrl1)
+//
+//            guard let imageTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else {
+//                assertionFailure()
+//                return
+//            }
+//            try! imageTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: firstAsset.duration), of: imageAsset.tracks(withMediaType: .video)[0], at: .zero)
+//
+//            // add layer instruction for first video
+//            let imageVideoLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: imageTrack)
+//
+//            // size = 486 widh 154
+//            // full size = CGSize(width: 640, height: 480)?
+//
+//            let imageScale = CGAffineTransform(scaleX: 640 / 486, y: 486 / 154)
+//
+//            imageVideoLayerInstruction.setTransform(imageScale, at: .zero)
+//
+//            mainInstruction.layerInstructions.append(imageVideoLayerInstruction)
+//
+//        }
 
-        // add layer instruction for first video
-        let firstVideoLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: firstTrack)
-        let firstMove = CGAffineTransform(translationX: 1000, y: 400)
-        let firstScale = CGAffineTransform(scaleX: 0.2, y: 0.2)
-
-        firstVideoLayerInstruction.setTransform(firstMove.concatenating(firstScale), at: .zero)
-
-        mainInstruction.layerInstructions.append(firstVideoLayerInstruction)
-
-
-        // make second video track and add to composition
-        let secondAsset = AVAsset(url: pathUrl)
-
-        guard let secondTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else {
-            assertionFailure()
-            return
-        }
-        try! secondTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: secondAsset.duration), of: secondAsset.tracks(withMediaType: .video)[0], at: .zero)
-
-        // add layer instruction for second video
-        let secondVideoLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: secondTrack)
-        let secondMove = CGAffineTransform(translationX: -500, y: -500)
-        let secondScale = CGAffineTransform(scaleX: 0.2, y: 0.2)
-
-        secondVideoLayerInstruction.setTransform(secondMove.concatenating(secondScale), at: .zero)
-
-        mainInstruction.layerInstructions.append(secondVideoLayerInstruction)
-
-
-        // make third video track and add to composition
-        let thirdAsset = AVAsset(url: pathUrl)
-
-        guard let thirdTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else {
-            assertionFailure()
-            return
-        }
-        try! thirdTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: thirdAsset.duration), of: thirdAsset.tracks(withMediaType: .video)[0], at: .zero)
-
-        // add layer instruction for third video
-        let thirdVideoLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: thirdTrack)
-        let thirdMove = CGAffineTransform(translationX: 0, y: 1000)
-        let thirdScale = CGAffineTransform(scaleX: 0.2, y: 0.2)
-
-        thirdVideoLayerInstruction.setTransform(thirdMove.concatenating(thirdScale), at: .zero)
-
-        mainInstruction.layerInstructions.append(thirdVideoLayerInstruction)
 
 
         // make video composition
@@ -120,11 +127,48 @@ class ViewController: UIViewController {
         exporter.outputURL = outputUrl
 
         exporter.exportAsynchronously {
-            DispatchQueue.main.async { [weak self] in
-                let endTime = DispatchTime.now()
-                print("time required: \((endTime.uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000_000 )s")
-                self?.didFinish(session: exporter)
+            DispatchQueue.main.async { //[weak self] in
+//                let endTime = DispatchTime.now()
+//                print("time required: \((endTime.uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000_000 )s")
+                completion(exporter)
             }
+        }
+    }
+
+    private func add(avAsset: AVAsset) {
+
+        guard let track = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else {
+            assertionFailure()
+            return
+        }
+        try! track.insertTimeRange(CMTimeRangeMake(start: .zero, duration: avAsset.duration), of: avAsset.tracks(withMediaType: .video)[0], at: .zero)
+
+        // add layer instruction for first video
+        let videoLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: track)
+        let move = CGAffineTransform(translationX: 1000, y: 400)
+        let scale = CGAffineTransform(scaleX: 0.2, y: 0.2)
+
+        videoLayerInstruction.setTransform(move.concatenating(scale), at: .zero)
+
+        mainInstruction.layerInstructions.append(videoLayerInstruction)
+    }
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let pathUrl = Bundle.main.url(forResource: "SampleVideo_1280x720_1mb", withExtension: "mp4") else {
+            assertionFailure()
+            return
+        }
+
+        // make second video track and add to composition
+        let asset = AVAsset(url: pathUrl)
+
+        VideoComposer().overlapVideos(avAsset: asset) { [weak self] exporter in
+            self?.didFinish(session: exporter)
         }
     }
 
@@ -145,6 +189,83 @@ class ViewController: UIViewController {
 
         self.present(player, animated: true) {
             self.player.player?.play()
+        }
+    }
+
+}
+
+class ImageVideoCreator {
+
+    private static func pixelBuffer(fromImage image: CGImage, size: CGSize) -> CVPixelBuffer? {
+        let options: CFDictionary = [kCVPixelBufferCGImageCompatibilityKey as String: true, kCVPixelBufferCGBitmapContextCompatibilityKey as String: true] as CFDictionary
+        var pxbuffer: CVPixelBuffer? = nil
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(size.width), Int(size.height), kCVPixelFormatType_32ARGB, options, &pxbuffer)
+
+        guard let buffer = pxbuffer, status == kCVReturnSuccess else {
+            return nil
+        }
+
+        CVPixelBufferLockBaseAddress(buffer, [])
+        guard let pxdata = CVPixelBufferGetBaseAddress(buffer) else {
+            return nil
+        }
+
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(buffer)
+
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: pxdata, width: Int(size.width), height: Int(size.height), bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+            return nil
+        }
+        context.concatenate(CGAffineTransform(rotationAngle: 0))
+        context.draw(image, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+
+        CVPixelBufferUnlockBaseAddress(buffer, [])
+
+        return buffer
+    }
+
+
+    static func writeSingleImageToMovie(image: UIImage, movieLength: TimeInterval, outputFileURL: URL, completion: @escaping (Bool) -> ()) {
+        do {
+            let imageSize = image.size
+            let videoWriter = try AVAssetWriter(outputURL: outputFileURL, fileType: AVFileType.mov)
+            let videoSettings: [String: Any] = [AVVideoCodecKey: AVVideoCodecH264,
+                                                AVVideoWidthKey: imageSize.width,
+                                                AVVideoHeightKey: imageSize.height]
+            let videoWriterInput = AVAssetWriterInput(mediaType: AVMediaType.video, outputSettings: videoSettings)
+            let adaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: videoWriterInput, sourcePixelBufferAttributes: nil)
+
+            if !videoWriter.canAdd(videoWriterInput) {
+                completion(false)
+                return
+            }
+            videoWriterInput.expectsMediaDataInRealTime = true
+            videoWriter.add(videoWriterInput)
+
+            videoWriter.startWriting()
+            let timeScale: Int32 = 600 // recommended in CMTime for movies.
+            let halfMovieLength = Float64(movieLength/2.0) // videoWriter assumes frame lengths are equal.
+            let startFrameTime = CMTimeMake(value: 0, timescale: timeScale)
+            let endFrameTime = CMTimeMakeWithSeconds(halfMovieLength, preferredTimescale: timeScale)
+            videoWriter.startSession(atSourceTime: startFrameTime)
+
+            guard let cgImage = image.cgImage else {
+                completion(false)
+                return
+            }
+            let buffer: CVPixelBuffer = self.pixelBuffer(fromImage: cgImage, size: imageSize)!
+            while !adaptor.assetWriterInput.isReadyForMoreMediaData { usleep(10) }
+            adaptor.append(buffer, withPresentationTime: startFrameTime)
+            while !adaptor.assetWriterInput.isReadyForMoreMediaData { usleep(10) }
+            adaptor.append(buffer, withPresentationTime: endFrameTime)
+
+            videoWriterInput.markAsFinished()
+            videoWriter.finishWriting {
+                // videoWriter.error
+                completion(true)
+            }
+        } catch {
+            completion(false)
         }
     }
 }
